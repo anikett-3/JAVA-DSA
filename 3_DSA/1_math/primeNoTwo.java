62b67c9b36516dd8adb26b0f99b248d19728710c
aniket <aniket3pd@gmail.com>
2026-07-07 12:37:02 +0530

    solving  basic math question  - prime no in range

---
+++ 3_DSA/1_math/primeNoTwo.java
@@ -0,0 +1,29 @@
+// HERE we gonna print Prime no from 1 to N or in range .
+public class primeNoTwo {
+    // first we have to write code for check a single number is prime or not
+    public static boolean checkPrimeNo(int num){
+        if(num<2){
+            return false;
+        }
+        for(int i = 2; i*i <= num; i++){
+            if (num% i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Here our main code for print prime number in range. 
+    public static void primeInRange(int n){
+        for(int i =2; i<=n-1;i++){
+            if(checkPrimeNo(i)){   // here we use checkprime function to check each number 2 to n. // and if check prime == true (default ) then it goes to next line if false if block will stopp.
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int num = 10;
+        primeInRange(num);
+
+    }
+}
