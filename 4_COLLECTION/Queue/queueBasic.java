db6ed17f4f03b29a7e9941f42abd1da37b90c723
aniket <aniket3pd@gmail.com>
2026-07-10 12:59:16 +0530

    learn some methods of vector class of List interface

---
+++ 4_COLLECTION/Queue/queueBasic.java
@@ -0,0 +1,14 @@
+package Queue;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class queueBasic {
+    public static void main(String[] args) {
+        Queue <Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(90);
+    } 
+    
+}
