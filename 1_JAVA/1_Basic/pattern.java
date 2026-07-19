87ccf5d9fd3882770c3466aa8508e1e1c1a7807b
aniket <aniket3pd@gmail.com>
2026-07-19 22:12:25 +0530

    practice more question of pattern

--- 1_JAVA/1_Basic/pattern.java
+++ 1_JAVA/1_Basic/pattern.java
@@ -1,7 +1,11 @@
 
 public class pattern {
     public static void main(String[] args) {
-            System.out.println("hello");
+            // System.out.println("hello");
+            System.out.println("****");
+            System.out.println("****");
+            System.out.println("****");
+            System.out.println("****");
         }
 
 
